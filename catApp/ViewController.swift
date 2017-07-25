c9cba324e8ad888fbce313076ebc3efa4c814772
Deb <deb@users-macbook-pro.local>
2017-07-25 14:25:35 -0500

    Initial

--- catApp/ViewController.swift
+++ catApp/ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    
+    @IBAction func submitPressed(_ sender: Any) {
+        // Test that everything works, remove once successful.
+        print(ageTextField.text as Any)
+        
+        if let tempAge = ageTextField.text {
+            let ageInCatYears = Int (tempAge)! * 7
+            
+            // Convert value to String using String ()
+            // Assign the string to ageLabel.text
+            ageLabel.text = String (ageInCatYears)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
